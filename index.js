47b3ebcfb77a5e2a1bde2a01a27c7558db26d233
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-08-30 16:38:50 -0400

    lint + fmt cleanup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  	MongosManager: require('./lib/tools/mongos_manager')
-  , ReplSetManager: require('./lib/tools/replset_manager')
-  , ServerManager: require('./lib/tools/server_manager')
-  , ShardingManager: require('./lib/tools/sharding_manager')
-}
+  MongosManager: require('./lib/tools/mongos_manager'),
+  ReplSetManager: require('./lib/tools/replset_manager'),
+  ServerManager: require('./lib/tools/server_manager'),
+  ShardingManager: require('./lib/tools/sharding_manager')
+};
